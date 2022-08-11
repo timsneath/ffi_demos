7051a547fb97c32c1d05a9cb0ab4f9ccac96df46
Tim Sneath <timsneath@google.com>
2022-08-11 12:51:40 -0700

    Fix pubspecs

---
+++ rawmode_c/enable_raw.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <termios.h>
+
+/* Raw mode: 1960 magic shit. */
+int enableRawMode(int fd)
+{
+    struct termios raw;
+
+    if (E.rawmode)
+        return 0; /* Already enabled. */
+    if (!isatty(STDIN_FILENO))
+        goto fatal;
+    atexit(editorAtExit);
+    if (tcgetattr(fd, &orig_termios) == -1)
+        goto fatal;
+
+    raw = orig_termios; /* modify the original mode */
+    /* input modes: no break, no CR to NL, no parity check, no strip char,
+     * no start/stop output control. */
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    /* output modes - disable post processing */
+    raw.c_oflag &= ~(OPOST);
+    /* control modes - set 8 bit chars */
+    raw.c_cflag |= (CS8);
+    /* local modes - choing off, canonical off, no extended functions,
+     * no signal chars (^Z,^C) */
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    /* control chars - set return condition: min number of bytes and timer. */
+    raw.c_cc[VMIN] = 0;  /* Return each byte, or zero for timeout. */
+    raw.c_cc[VTIME] = 1; /* 100 ms timeout (unit is tens of second). */
+
+    /* put terminal in raw mode after flushing */
+    if (tcsetattr(fd, TCSAFLUSH, &raw) < 0)
+        goto fatal;
+    E.rawmode = 1;
+    return 0;
+
+fatal:
+    errno = ENOTTY;
+    return -1;
+}
--- rawmode_c/rawmode_win.cpp
+++ rawmode_c/rawmode_win.cpp
@@ -1,7 +1,7 @@
 #define WIN32_LEAN_AND_MEAN
 
-#include <Windows.h>
 #include <stdio.h>
+#include <Windows.h>
 
 void enableRawMode()
 {
