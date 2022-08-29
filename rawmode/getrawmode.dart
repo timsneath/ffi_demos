70e444b77a2e282ffc4e250f952db3126b27d5eb
Tim Sneath <timsneath@google.com>
2022-08-29 01:12:22 -0700

    Clean up example

--- rawmode/getrawmode.dart
+++ rawmode/getrawmode.dart
@@ -7,42 +7,26 @@ import 'package:ffi/ffi.dart';
 
 import 'stdin_raw.dart';
 
-typedef GetConsoleModeC = Int32 Function(IntPtr, Pointer<Int32>);
-typedef GetConsoleModeDart = int Function(int, Pointer<Int32>);
-
-final kernel32 = DynamicLibrary.open('kernel32.dll');
-final getConsoleMode = kernel32
-    .lookupFunction<GetConsoleModeC, GetConsoleModeDart>('GetConsoleMode');
-
-const STD_INPUT_HANDLE = -10;
-const ENABLE_PROCESSED_INPUT = 0x0001;
-const ENABLE_LINE_INPUT = 0x0002;
-const ENABLE_ECHO_INPUT = 0x0004;
-const ENABLE_WINDOW_INPUT = 0x0008;
-const ENABLE_VIRTUAL_TERMINAL_INPUT = 0x0200;
-
 void printConsoleFlags(int flags) {
   print('Echo: ${flags & ENABLE_ECHO_INPUT == ENABLE_ECHO_INPUT}');
   print('Line input: ${flags & ENABLE_LINE_INPUT == ENABLE_LINE_INPUT}');
   print(
       'Process input: ${flags & ENABLE_PROCESSED_INPUT == ENABLE_PROCESSED_INPUT}');
   print(
-      'Console resize events: ${flags & ENABLE_WINDOW_INPUT == ENABLE_PROCESSED_INPUT}');
-  print(
-      'VT escape sequence input: ${flags & ENABLE_VIRTUAL_TERMINAL_INPUT == ENABLE_VIRTUAL_TERMINAL_INPUT}\n');
+      'Quick edit mode: ${flags & ENABLE_QUICK_EDIT_MODE == ENABLE_QUICK_EDIT_MODE}');
 }
 
 void main() {
-  final stdInHandle = getStdHandle(STD_INPUT_HANDLE);
+  final stdInHandle = GetStdHandle(STD_INPUT_HANDLE);
 
   // Regular console mode
   final lpMode = calloc<Int32>();
-  getConsoleMode(stdInHandle, lpMode);
+  GetConsoleMode(stdInHandle, lpMode);
   printConsoleFlags(lpMode.value);
 
   // Raw console mode
   stdin.rawMode = true;
-  getConsoleMode(stdInHandle, lpMode);
+  GetConsoleMode(stdInHandle, lpMode);
   printConsoleFlags(lpMode.value);
 
   calloc.free(lpMode);
--- rawmode/stdin_raw.dart
+++ rawmode/stdin_raw.dart
@@ -1,25 +1,52 @@
 import 'dart:ffi';
 import 'dart:io';
 
-typedef GetStdHandleC = IntPtr Function(Int32);
-typedef GetStdHandleDart = int Function(int);
-
-typedef SetConsoleModeC = Int32 Function(IntPtr, Int32);
-typedef SetConsoleModeDart = int Function(int, int);
-
+// The Windows kernel library, which exports these functions
 final kernel32 = DynamicLibrary.open('kernel32.dll');
-final getStdHandle =
-    kernel32.lookupFunction<GetStdHandleC, GetStdHandleDart>('GetStdHandle');
-final setConsoleMode = kernel32
-    .lookupFunction<SetConsoleModeC, SetConsoleModeDart>('SetConsoleMode');
 
+// HANDLE GetStdHandle(DWORD nStdHandle)
+typedef GetStdHandle_C = IntPtr Function(Int32);
+typedef GetStdHandle_Dart = int Function(int);
+final GetStdHandle =
+    kernel32.lookupFunction<GetStdHandle_C, GetStdHandle_Dart>('GetStdHandle');
+
+// BOOL GetConsoleMode(HANDLE hConsoleHandle, LPDWORD lpMode)
+typedef GetConsoleMode_C = Int32 Function(IntPtr, Pointer<Int32>);
+typedef GetConsoleMode_Dart = int Function(int, Pointer<Int32>);
+final GetConsoleMode = kernel32
+    .lookupFunction<GetConsoleMode_C, GetConsoleMode_Dart>('GetConsoleMode');
+
+// BOOL SetConsoleMode(HANDLE hConsoleHandle, DWORD dwMode)
+typedef SetConsoleMode_C = Int32 Function(IntPtr, Int32);
+typedef SetConsoleMode_Dart = int Function(int, int);
+final SetConsoleMode = kernel32
+    .lookupFunction<SetConsoleMode_C, SetConsoleMode_Dart>('SetConsoleMode');
+
+const STD_INPUT_HANDLE = -10;
+const ENABLE_PROCESSED_INPUT = 0x0001;
+const ENABLE_LINE_INPUT = 0x0002;
+const ENABLE_ECHO_INPUT = 0x0004;
+const ENABLE_MOUSE_INPUT = 0x0010;
+const ENABLE_INSERT_MODE = 0x0020;
+const ENABLE_QUICK_EDIT_MODE = 0x0040;
+
+// Use package:dart_console for a more polished version.
 extension SetRawMode on Stdin {
   set rawMode(bool isRaw) {
-    final stdin = getStdHandle(-10 /* STD_INPUT_HANDLE */);
+    final stdin = GetStdHandle(STD_INPUT_HANDLE);
+
     if (isRaw) {
-      setConsoleMode(stdin, 0x70 /* 'raw' mode */);
+      SetConsoleMode(stdin,
+          ENABLE_QUICK_EDIT_MODE | ENABLE_INSERT_MODE | ENABLE_MOUSE_INPUT);
     } else {
-      setConsoleMode(stdin, 0x77 /* 'normal' mode */);
+      SetConsoleMode(
+          stdin,
+          ENABLE_QUICK_EDIT_MODE |
+              ENABLE_INSERT_MODE |
+              ENABLE_MOUSE_INPUT |
+              ENABLE_ECHO_INPUT |
+              ENABLE_LINE_INPUT |
+              ENABLE_PROCESSED_INPUT);
     }
   }
 }
