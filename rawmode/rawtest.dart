5c39a43a3337dec060f4f9503c7c635f34762f55
Tim Sneath <tim@sneath.org>
2022-09-02 09:17:26 +0200

    Fix ordering

--- rawmode/rawtest.dart
+++ rawmode/rawtest.dart
@@ -4,11 +4,11 @@ import 'stdin_raw.dart';
 void main() {
   stdout.write('Enter some text (normal mode): ');
   final textNormal = stdin.readLineSync();
-  stdin.rawMode = true;
   stdout.write('You wrote $textNormal\n\n');
 
+  stdin.rawMode = true;
   stdout.write('Enter some text (raw mode): ');
   final textRaw = stdin.readLineSync();
-  stdin.rawMode = false;
   stdout.write('You wrote $textRaw\n\n');
+  stdin.rawMode = false;
 }
